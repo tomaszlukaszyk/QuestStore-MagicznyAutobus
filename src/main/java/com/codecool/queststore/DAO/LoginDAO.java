33c7901241179bae6a21d43dabeb992f54458e56
Czakero <bartosz.pyrz96@gmail.com>
2018-07-30 15:07:07 +0200

    Validation parameter changed

--- src/main/java/com/codecool/queststore/DAO/LoginDAO.java
+++ src/main/java/com/codecool/queststore/DAO/LoginDAO.java
@@ -1,22 +1,22 @@
 package com.codecool.queststore.DAO;
+import com.codecool.queststore.model.Login;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 public class LoginDAO  implements Connectable {
 
     private final static int USER_ID = 1;
 
-    public int validation(String userName, String password) throws SQLException {
+    public int validation(Login login) throws SQLException {
         int result = 0;
         Connection conn = cp.getConnection();
         cp.printDbStatus();
-        PreparedStatement stmt = conn.prepareStatement("SELECT validation(?, ?)");
-        stmt.setString(1, userName);
-        stmt.setString(2, password);
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM loginValidation(?, ?)");
+        stmt.setString(1, login.getLOGIN());
+        stmt.setString(2, login.getPASSWORD());
         ResultSet rs = stmt.executeQuery();
         while (rs.next()) {
             result = rs.getInt(USER_ID);
