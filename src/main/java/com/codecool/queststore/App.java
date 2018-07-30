b2a29887d51a76f0df5c473639cd49def231b53b
Czakero <bartosz.pyrz96@gmail.com>
2018-07-30 15:12:07 +0200

    App for testing dao

--- src/main/java/com/codecool/queststore/App.java
+++ src/main/java/com/codecool/queststore/App.java
@@ -1,16 +1,29 @@
 package com.codecool.queststore;
 
+import com.codecool.queststore.DAO.UserDAO;
+import com.codecool.queststore.model.Login;
 import com.codecool.queststore.model.server.Server;
+import com.codecool.queststore.model.user.Role;
+import com.codecool.queststore.model.user.User;
+
+import java.sql.SQLException;
 
 public class App {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws SQLException {
         Server server = new Server();
         try {
             server.run();
         }catch (Exception e){
             System.out.println(e.getStackTrace());
         }
+
+        UserDAO dao = new UserDAO();
+        Login login = new Login("baba", "lolo");
+        User user = new User("tak", "tak", "tak", "cracow", 0, Role.MENTOR);
+
+
+
     }
 
 }
