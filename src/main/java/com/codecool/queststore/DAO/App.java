5f78c4d9ef352abed5bee8f5ca155ce161c669dd
Czakero <bartosz.pyrz96@gmail.com>
2018-07-30 14:25:11 +0200

    App class for testing user dao methods

--- src/main/java/com/codecool/queststore/DAO/App.java
+++ src/main/java/com/codecool/queststore/DAO/App.java
@@ -1,7 +1,7 @@
 package com.codecool.queststore.DAO;
-
-
-
+import com.codecool.queststore.model.Login;
+import com.codecool.queststore.model.user.Role;
+import com.codecool.queststore.model.user.User;
 
 import java.sql.SQLException;
 
@@ -9,7 +9,11 @@ public class App {
     public static void main(String[] args) throws SQLException {
         LoginDAO login = new LoginDAO();
         UserDAO user = new UserDAO();
-        System.out.println(login.validation("testStudent2", "123"));
+        Login loginn = new Login("testStudent2", "123");
+        System.out.println(login.validation(loginn));
         user.deleteUser(1);
+        System.out.println(user.getUser(2).toString());
+        User userr = new User("nie", "nie", "nie", "nie", 3, Role.STUDENT);
+        user.updateUser(userr);
     }
 }
