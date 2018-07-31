4ff0b41b7b78ee54e3c16eab9544377685c8bb0f
Czakero <bartosz.pyrz96@gmail.com>
2018-07-31 15:49:51 +0200

    Implement post method handler

--- src/main/java/com/codecool/queststore/controller/server/httphandler/MentorsHandler.java
+++ src/main/java/com/codecool/queststore/controller/server/httphandler/MentorsHandler.java
@@ -1,19 +1,27 @@
 package com.codecool.queststore.controller.server.httphandler;
 
+import com.codecool.queststore.DAO.MentorDAO;
+import com.codecool.queststore.model.Login;
+import com.codecool.queststore.model.user.Role;
+import com.codecool.queststore.model.user.User;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.net.HttpCookie;
 import java.sql.SQLException;
+import java.util.Map;
 
 public class MentorsHandler extends AbstractHttphandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        System.out.println(httpExchange.getRequestMethod());
-
         try {
-            handleSession(httpExchange);
+            if(httpExchange.getRequestMethod().equals("POST")){
+                handlePost(httpExchange);
+            } else{
+                handleSession(httpExchange);
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -30,9 +38,23 @@ public class MentorsHandler extends AbstractHttphandler implements HttpHandler {
 
             } else {
                 MentorsService mentorsService = new MentorsService(cookie);
-                String response = mentorsService.generateResponseBody();
+                String response = mentorsService.generateResponseBody(true);
                 SendReq(httpExchange, response);
             }
+        }
+
+        private void handlePost(HttpExchange httpExchange) throws IOException, SQLException {
+            String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+            HttpCookie cookie = new HttpCookie("Session-id", cookieStr);
+            MentorsService mentorsService = new MentorsService(cookie);
+            Map inputs = parseFormData(httpExchange);
+            MentorDAO dao = new MentorDAO();
+            boolean isCreated = dao.createMentor(new User(inputs.get("mentorname").toString(), inputs.get("mentorsurname").toString(),
+                                      inputs.get("mentoremail").toString(), inputs.get("mentoraddress").toString(), 0, Role.MENTOR),
+                                        new Login(inputs.get("mentorlogin").toString(), inputs.get("mentorpassword").toString()));
+
+            String response =  mentorsService.generateResponseBody(isCreated);
+            SendReq(httpExchange, response);
         }
     }
 
