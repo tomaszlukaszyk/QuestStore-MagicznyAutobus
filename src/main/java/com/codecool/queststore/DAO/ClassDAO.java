616ad9c81b677ef9da90a0038c468c9c89353f8f
Czakero <bartosz.pyrz96@gmail.com>
2018-08-01 16:00:53 +0200

    Methods changed, closing every connection now, little clean code

--- src/main/java/com/codecool/queststore/DAO/ClassDAO.java
+++ src/main/java/com/codecool/queststore/DAO/ClassDAO.java
@@ -9,35 +9,30 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ClassDAO implements ClassDAOInterface, Connectable{
+public class ClassDAO implements ClassDAOInterface, Connectable {
     @Override
-    public List<CodecoolClass> getClasses() {
+    public List<CodecoolClass> getClasses() throws SQLException {
         List<CodecoolClass> classes = new ArrayList<>();
-        try {
-            Connection conn = cp.getConnection();
-            cp.printDbStatus();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM getClasses()");
+        Connection conn = cp.getConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM getClasses()");
 
-            while (rs.next()) {
-                int classID = rs.getInt("id");
-                String name = rs.getString("description");
-                classes.add(new CodecoolClass(classID, name, getMentors(classID), getStudents(classID)));
-            }
-
-            return classes;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
+        while (rs.next()) {
+            classes.add(new CodecoolClass(rs.getInt("id"), rs.getString("description"),
+                        getMentorsFromClass(rs.getInt("id")), getStudentsFromClass(rs.getInt("id"))));
         }
+
+        rs.close();
+        conn.close();
+
+        return classes;
     }
 
-    private List<User> getMentors(int idClass) throws SQLException{
-        List<User> users = new ArrayList<>();
+    public List<User> getMentorsFromClass(int classId) throws SQLException{
+        List<User> result = new ArrayList<>();
         Connection conn = cp.getConnection();
-        cp.printDbStatus();
         PreparedStatement stmt = conn.prepareStatement("SELECT * FROM getClassMentors(?)");
-        stmt.setInt(1, idClass);
+        stmt.setInt(1, classId);
         ResultSet rs = stmt.executeQuery();
 
         while (rs.next()) {
@@ -47,18 +42,18 @@ public class ClassDAO implements ClassDAOInterface, Connectable{
             String email = rs.getString("email");
             String address = rs.getString("address");
 
-            users.add(new User(name, surname, email, address, id, Role.MENTOR));
+            result.add(new User(name, surname, email, address, id, Role.MENTOR));
         }
-
-        return users;
+        rs.close();
+        conn.close();
+        return result;
     }
 
-    private List<User> getStudents(int idClass) throws SQLException{
-        List<User> users = new ArrayList<>();
+    public List<User> getStudentsFromClass(int classId) throws SQLException {
+        List<User> result = new ArrayList<>();
         Connection conn = cp.getConnection();
-        cp.printDbStatus();
         PreparedStatement stmt = conn.prepareStatement("SELECT * FROM getClassStudents(?)");
-        stmt.setInt(1, idClass);
+        stmt.setInt(1, classId);
         ResultSet rs = stmt.executeQuery();
 
         while (rs.next()) {
@@ -68,9 +63,10 @@ public class ClassDAO implements ClassDAOInterface, Connectable{
             String email = rs.getString("email");
             String address = rs.getString("address");
 
-            users.add(new User(name, surname, email, address, id, Role.STUDENT));
+            result.add(new User(name, surname, email, address, id, Role.STUDENT));
         }
-
-        return users;
+        rs.close();
+        conn.close();
+        return result;
     }
 }
