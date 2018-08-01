e9e4bdf386842763509fa80471c867e2f2ae3ef1
Czakero <bartosz.pyrz96@gmail.com>
2018-08-01 13:39:08 +0200

    Method paramter changed

--- src/main/java/com/codecool/queststore/view/TemplateModelInterface.java
+++ src/main/java/com/codecool/queststore/view/TemplateModelInterface.java
@@ -14,7 +14,7 @@ interface TemplateModelInterface {
     JtwigModel getClassModel(User currentUser, List<CodecoolClass> classes);
     JtwigModel getProfileStudentModel(User currentUser, User profile, CodecoolClass ccClass, List<Artifact> items);
     JtwigModel getProfileMentorModel(User currentUser, User profile, List<CodecoolClass> classes);
-    JtwigModel getStudentsListModel(User currentUser, List<User> users);
+    JtwigModel getStudentsListModel(User currentUser, List<User> users, boolean isCreated);
     JtwigModel getMentorsListModel(User currentUser, List<User> users, boolean isCreated);
     JtwigModel getQuestModel(User currentUser, List<Quest> quests);
     JtwigModel getArtifactModel(User currentUser, List<Artifact> artifacts);
