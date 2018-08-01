35b6005871e5131f307f81a9140160a136a4d762
Czakero <bartosz.pyrz96@gmail.com>
2018-08-01 13:51:50 +0200

    Fix merge

--- src/main/java/com/codecool/queststore/model/server/Server.java
+++ src/main/java/com/codecool/queststore/model/server/Server.java
@@ -1,6 +1,7 @@
 package com.codecool.queststore.model.server;
 
 import com.codecool.queststore.controller.server.httphandler.*;
+import com.codecool.queststore.controller.server.httphandler.codecoolClass.ClassHandler;
 import com.codecool.queststore.controller.server.httphandler.profile.ProfileHandler;
 import com.codecool.queststore.model.server.session.SessionPool;
 import com.sun.net.httpserver.HttpServer;
@@ -35,6 +36,7 @@ public class Server {
         server.createContext("/static", new StaticHandler());
         server.createContext("/list-mentors", new MentorsHandler());
         server.createContext("/list-students", new StudentsHandler());
+        server.createContext("/class", new ClassHandler());
     }
 
     public void stop(){
