f99601393686db59fde3bab989ccbf63ef8571fc
Czakero <bartosz.pyrz96@gmail.com>
2018-08-01 16:01:54 +0200

    ClassDAOInterface new methods implemented

--- src/main/java/com/codecool/queststore/dao/interfaces/ClassDAOInterface.java
+++ src/main/java/com/codecool/queststore/dao/interfaces/ClassDAOInterface.java
@@ -1,9 +1,13 @@
 package com.codecool.queststore.dao.interfaces;
 
 import com.codecool.queststore.model.classes.CodecoolClass;
+import com.codecool.queststore.model.user.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ClassDAOInterface {
-    List<CodecoolClass> getClasses();
+    List<CodecoolClass> getClasses() throws SQLException;
+    List<User> getMentorsFromClass(int classId) throws SQLException;
+    List<User> getStudentsFromClass(int classId) throws SQLException;
 }
