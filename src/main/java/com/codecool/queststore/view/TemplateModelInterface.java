5c2491f20c3b8c71138ce1e2c129588848b07f0e
Czakero <bartosz.pyrz96@gmail.com>
2018-08-02 11:02:27 +0200

    Fix bug

--- src/main/java/com/codecool/queststore/view/TemplateModelInterface.java
+++ src/main/java/com/codecool/queststore/view/TemplateModelInterface.java
@@ -1,5 +1,6 @@
 package com.codecool.queststore.view;
 
+import com.codecool.queststore.model.Title;
 import com.codecool.queststore.model.classes.CodecoolClass;
 import com.codecool.queststore.model.shop.artifact.Artifact;
 import com.codecool.queststore.model.shop.quest.Quest;
@@ -15,6 +16,7 @@ interface TemplateModelInterface {
     JtwigModel getClassModel(User currentUser, List<CodecoolClass> classes, CodecoolClass targetClass);
     JtwigModel getProfileStudentModel(User currentUser, User profile, CodecoolClass ccClass, List<Artifact> items);
     JtwigModel getProfileMentorModel(User currentUser, User profile, List<CodecoolClass> classes);
+    JtwigModel getProfileAdminModel(User currentUser, List<Title> titles);
     JtwigModel getStudentsListModel(User currentUser, List<User> users, boolean isCreated, List<CodecoolClass> classes);
     JtwigModel getMentorsListModel(User currentUser, List<User> users, boolean isCreated);
     JtwigModel getQuestModel(User currentUser, List<Quest> quests);
