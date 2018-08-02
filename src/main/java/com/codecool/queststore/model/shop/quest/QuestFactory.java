5c25772961098e607ae8101dbd92b619e660b40f
Pieczkowski <pieczkowski.t@gmail.com>
2018-08-02 20:47:56 +0200

    Implement QuestFactory

--- src/main/java/com/codecool/queststore/model/shop/quest/QuestFactory.java
+++ src/main/java/com/codecool/queststore/model/shop/quest/QuestFactory.java
@@ -1,24 +1,29 @@
 package com.codecool.queststore.model.shop.quest;
 
-import com.codecool.queststore.dao.QuestDAO;
-import com.codecool.queststore.model.shop.artifact.Artifact;
-import com.codecool.queststore.model.shop.artifact.ArtifactCategory;
+import com.codecool.queststore.DAO.QuestDAO;
+import com.codecool.queststore.dao.interfaces.QuestDAOInterface;
 
 import java.util.List;
 
 public class QuestFactory {
 
     public List<Quest> listFromUserID(int id) {
-        QuestDAO dao = new QuestDAO();
+        QuestDAOInterface dao = new QuestDAO();
         return dao.getAllQuestsByUser(id);
     }
 
-    public QuestTemplate templatefromData(String name, String description, int value, String imageName, String imageMarkedName, QuestCategory category){
-        return new QuestTemplate(name, description, value, imageName, imageMarkedName, category);
+    public List<QuestTemplate> getTemplates() {
+        QuestDAOInterface dao = new QuestDAO();
+        return dao.getAllQuestTemplates();
     }
 
-    public QuestTemplate templatefromData(String name, String description, int value, String imageName, String imageMarkedName, int categoryVal){
-        return new QuestTemplate(name, description, value, imageName, imageMarkedName, categoryVal);
+
+    public QuestTemplate templatefromData(int ID, String name, String description, int value, String imageName, String imageMarkedName, QuestCategory category){
+        return new QuestTemplate(ID, name, description, value, imageName, imageMarkedName, category);
+    }
+
+    public QuestTemplate templatefromData(int ID, String name, String description, int value, String imageName, String imageMarkedName, int categoryVal){
+        return new QuestTemplate(ID, name, description, value, imageName, imageMarkedName, categoryVal);
     }
 
     public Quest questfromTemplate(QuestTemplate questTemplate, boolean isDone, int historyID, int studentID){
