ede30e622d09bcc79986c9ba3d3b5404cc9c3fdd
Czakero <bartosz.pyrz96@gmail.com>
2018-08-02 14:16:31 +0200

    Fix bugs

--- src/main/java/com/codecool/queststore/view/TemplateModelInterface.java
+++ src/main/java/com/codecool/queststore/view/TemplateModelInterface.java
@@ -6,10 +6,8 @@ import com.codecool.queststore.model.shop.artifact.Artifact;
 import com.codecool.queststore.model.shop.quest.Quest;
 import com.codecool.queststore.model.user.User;
 import org.jtwig.JtwigModel;
-
-import javax.jws.soap.SOAPBinding;
 import java.util.List;
-import java.util.Map;
+
 
 interface TemplateModelInterface {
     JtwigModel getClassModel(User currentUser, List<CodecoolClass> classes);
