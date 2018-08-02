6edbe3d882875f8bb26e748ecbfbe82d4b75ceed
kret0 <mjp.sik@gmail.com>
2018-08-03 00:48:04 +0200

    adjust profile student to have link to his quests.

--- src/main/java/com/codecool/queststore/model/shop/artifact/ArtifactFactory.java
+++ src/main/java/com/codecool/queststore/model/shop/artifact/ArtifactFactory.java
@@ -1,13 +1,8 @@
 package com.codecool.queststore.model.shop.artifact;
 
-import java.util.List;
-
 public class ArtifactFactory {
 
-    public List<Artifact> listFromUserID(int id) {
-        //todo: dao pullin all artifacts
-        return null;
-    }
+
     public Artifact fromData(int id, int historyid, String name, String description, int cost, String imageName, String imageMarkedName, ArtifactCategory category, boolean isUsed) {
         return new Artifact(id, historyid, name, description, cost, imageName, imageMarkedName, category, isUsed);
     }
