1924e266a59b2ef659848e63f74d4634bda70dd9
Czakero <bartosz.pyrz96@gmail.com>
2018-08-02 18:57:51 +0200

    Delete comments

--- src/main/java/com/codecool/queststore/model/server/Server.java
+++ src/main/java/com/codecool/queststore/model/server/Server.java
@@ -36,7 +36,6 @@ public class Server {
         server.createContext("/list-mentors", new MentorsHandler());
         server.createContext("/list-students", new StudentsHandler());
         server.createContext("/class", new ClassHandler());
-//        server.createContext("/edit", new EditHandler());
     }
 
     public void stop(){
